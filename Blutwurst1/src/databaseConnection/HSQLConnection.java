d4e499074ff91a718deb80356911ea76827d3b71
Oerni <rene.kirchhoff90@googlemail.com>
2013-09-12 14:06:58 +0200

    Integration HSQL Datenbank

--- Blutwurst1/src/databaseConnection/HSQLConnection.java
+++ Blutwurst1/src/databaseConnection/HSQLConnection.java
@@ -1,5 +1,49 @@
 package databaseConnection;
 
-public class HSQLConnection {
+import java.sql.*;
 
+public class HSQLConnection {
+	public HSQLConnection(){
+		try{
+			Class.forName("org.hsqldb.jdbcDriver");
+		}catch(ClassNotFoundException ex){
+			System.out.println("Treiber nicht gefunden!");
+			return;
+		}
+		
+		Connection con = null;
+		
+		try{
+			con = DriverManager.getConnection("jdbc:hsqldb:file:home/blutwurst/trash/hsql; shutdown=true", "root", "blutwurst1");
+			Statement state = con.createStatement();
+			
+			String query = "SELECT * FROM customer";
+			ResultSet rs = state.executeQuery(query);
+			
+			while(rs.next()){
+				String id = rs.getString(1);
+				String firstName = rs.getString(2);
+				String lastName = rs.getString(3);
+				System.out.println(id + ", " + firstName + ", " + lastName);
+			}
+			
+			rs.close();
+			state.close();
+		}catch(SQLException ex){
+			ex.printStackTrace();
+		}
+		
+		finally{
+			if(con != null){
+				try{
+					con.close();
+				}catch(SQLException ex){
+					ex.printStackTrace();
+				}
+			}
+		}
+	}
+	public static void main(String[] args){
+		new HSQLConnection();
+	}
 }
